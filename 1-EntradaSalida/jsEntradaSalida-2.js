f1848154ff6585c2e528267f7b8349bd65a185a1
ceroeiby <40859195+ceroeiby@users.noreply.github.com>
2018-07-05 11:25:05 -0300

    primer comit
    estos son los ejercicios realizados en la primera clase.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,23 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+//var persona =	prompt("Please enter your name", "Harry Potter");
+/*
+esto 
+es
+un 
+comentario
+*/
+/*
+var nombre;
+//nombre="Ivan";
+//alert (nombre);
+nombre =	prompt("ingrese su nombre");
+alert ("su nombre es:"+ nombre);
+*/
+var nombre;
+
+document.getElementById('elNombre');
+alert (nombre);
 }
 
